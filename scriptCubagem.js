bc2663ada2a4d06d6ed880a23c3cf908d5eba9f4
DiegoPauloTK147 <editordiegopaulo@gmail.com>
2025-03-13 17:11:23 -0300

    Inclusão botão resetar

--- scriptCubagem.js
+++ scriptCubagem.js
@@ -5,6 +5,8 @@ function botaoClicado() {
   let num1 = document.getElementById("altura").value;
   let num2 = document.getElementById("largura").value;
   let num3 = document.getElementById("comprimento").value;
+  let botaoCalcular = document.getElementById("botaoCalcular");
+  let botaoReset = document.getElementById("botaoReset");
 
   let resultOfMultiply = Number(num1) * Number(num2) * Number(num3);
 
@@ -19,9 +21,14 @@ function botaoClicado() {
   if (sector.value == "maritimo") {
     resultCubagem.innerHTML = Number(resultOfMultiply) * 1000;
   }
+
+  
+  botaoCalcular.style.display = "none";
+  botaoReset.style.display = "block";
 }
 
+function recarregarPagina() {
+  location.reload();
+}
 
-
-
-sector.addEventListener ("change", botaoClicado);
+sector.addEventListener("change", botaoClicado);
