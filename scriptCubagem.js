1aa7793c6cad155035855303394885266287107b
DiegoPauloTK147 <editordiegopaulo@gmail.com>
2025-03-12 21:47:02 -0300

    Finalizado

--- scriptCubagem.js
+++ scriptCubagem.js
@@ -1,3 +1,24 @@
+const sector = document.getElementById("sector")
+const resultCubagem = document.getElementById("resultCubagem")
+
 function botaoClicado() {
-    alert("OLA MUNDO")
+    let num1 = document.getElementById("altura").value;
+    let num2 = document.getElementById("largura").value;
+    let num3 = document.getElementById("comprimento").value;
+    
+
+    let resultOfMultiply = Number(num1) * Number(num2) * Number(num3);
+    
+    if ( sector.value == "rodoviario"){
+        resultCubagem.innerHTML = (Number(resultOfMultiply) * 300)
+    }
+
+    if ( sector.value == "aereo"){
+        resultCubagem.innerHTML = (Number(resultOfMultiply) * 167)
+    }
+
+    if ( sector.value == "maritimo"){
+        resultCubagem.innerHTML = (Number(resultOfMultiply) * 1000)
+    }
+
 }
