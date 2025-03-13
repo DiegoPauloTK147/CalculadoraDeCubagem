4d13043c5dd6455e04dd09c8fb44744b87cb6e60
DiegoPauloTK147 <editordiegopaulo@gmail.com>
2025-03-13 17:50:01 -0300

    Correção

--- scriptCubagem.js
+++ scriptCubagem.js
@@ -10,31 +10,30 @@ function botaoClicado() {
 
   let resultOfMultiply = Number(num1) * Number(num2) * Number(num3);
 
-
   if (num1=="" || num2=="" || num3==""){
     alert("Preencha os campos de metragem")
-
     location.reload();
   } else{
     if (sector.value == "rodoviario") {
       resultCubagem.innerHTML = Number(resultOfMultiply) * 300;
     }
-  
+
     if (sector.value == "aereo") {
       resultCubagem.innerHTML = Number(resultOfMultiply) * 167;
     }
-  
+
     if (sector.value == "maritimo") {
       resultCubagem.innerHTML = Number(resultOfMultiply) * 1000;
     }
 
     sector.addEventListener("change", botaoClicado);
   }
-  
-
 }
 
 function changeButton(){
+  let botaoCalcular = document.getElementById("botaoCalcular");
+  let botaoReset = document.getElementById("botaoReset");
+
   botaoCalcular.style.display = "none";
   botaoReset.style.display = "block";
 }
@@ -42,5 +41,3 @@ function changeButton(){
 function recarregarPagina() {
   location.reload();
 }
-
-
